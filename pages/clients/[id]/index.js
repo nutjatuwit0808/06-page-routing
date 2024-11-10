a273d70b489a1c4f2a607397ff9610825b8974c1
nutjatuwit <103878796+nutjatuwit0808@users.noreply.github.com>
2024-11-10 21:17:24 +0700

    update file-based routing

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,9 +1,26 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 function ClientProjectsPage() {
+    const router = useRouter();
+
+    console.log(router.query);
+
+    function loadProjectHandler() {
+        //load data ...
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: {
+                id: "nut",
+                clientprojectid: "projecta"
+            }
+        })
+    }
+    
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
